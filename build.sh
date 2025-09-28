53cc8548cd356fa7a6753b5964465d5e9e08075c
jared6484 <jared.6484@gmail.com>
2025-09-28 00:47:18 -0400

    docs

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-REPO_NAME="Static-Site-Generator"  # 📝 Change this to your actual repo name
+#REPO_NAME="Static-Site-Generator"  # 📝 Change this to your actual repo name
 
-echo "🔨 Building site for GitHub Pages at /Static-Site-Generator/"
+#echo "🔨 Building site for GitHub Pages at /Static-Site-Generator/"
 
-python3 src/main.py "/REPO_NAME/"
+python3 src/main.py "/Static-Site-Generator/"
